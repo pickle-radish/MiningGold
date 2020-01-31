cc63efb2e7d690490ea010c44fe3cce66f922bcc
Hogyeong Lee <beyondluiz@gmail.com>
2020-01-31 11:29:28 +0900

    1.0.1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const indexRouter=require('./routes/index');
 const express=require('express');
 const session=require('express-session');
+const path=require('path');
 const app=express();
 
 app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
