8ca2918cb35374324ce9c5bb4cb666cf54fae813
Hogyeong Lee <beyondluiz@gmail.com>
2020-01-31 16:51:40 +0900

    1.0.7

--- public/js/client.js
+++ public/js/client.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
     $(document).on('click', '#main_tab', function(){
-        $('#main_div').show();
+        $('#information_div').show();
         $('#market_div').hide();
         $('#inventory_div').hide();
     });
     $(document).on('click', '#market_tab', function(){
-        $('#main_div').hide();
+        $('#information_div').hide();
         $('#market_div').show();
         $('#inventory_div').hide();
         
     });
     $(document).on('click', '#inventory_tab', function(){
-        $('#main_div').hide();
+        $('#information_div').hide();
         $('#market_div').hide();
         $('#inventory_div').show();
     });
